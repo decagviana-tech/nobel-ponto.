a993a14d909bbc18028c07021c323796a4578d17
decagviana-tech <decagviana@gmail.com>
2026-02-03 16:38:03 -0300

    Delete nobel-ponto-inteligente (13) directory

--- nobel-ponto-inteligente (13)/componentes/TimeClock.tsx
+++
@@ -1,4 +0,0 @@
-
-// Este arquivo está obsoleto. O código foi movido para components/TimeClock.tsx
-// O Netlify pode estar tentando carregar este arquivo devido ao cache do Git.
-export {};
--- nobel-ponto-inteligente (13)/servicos/geminiService.ts
+++
@@ -1,3 +0,0 @@
-
-// Este arquivo está obsoleto. O código foi movido para services/geminiService.ts
-export {};
